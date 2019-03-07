246f074c10d2e96238375373f315fdd11f7070ed
Kate F <follett.katie@gmail.com>
2019-03-07 18:40:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length > 3
-    return array.insert(-2, "and").join(", ")
+    return array.insert(-1, "and").join(", ")
   end
 end
