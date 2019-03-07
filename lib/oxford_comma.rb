3f146e360bdb0a32580b9058bb05f5a9cfc1e58b
Kate F <follett.katie@gmail.com>
2019-03-07 18:45:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
+require 'pry'
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.first
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length > 2
+    array[-1].prepend "and "
+	  array.join(", ")
+  end
 end
